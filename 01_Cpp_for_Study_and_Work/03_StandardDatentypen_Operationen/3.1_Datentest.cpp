5901a1d02029d5c08763e807a8c72d28066380f3
Simon <simi.wiesinger@gamil.com>
2023-03-09 18:02:28 +0100

    Chapter 03 first Example

---
+++ 01_Cpp_for_Study_and_Work/03_StandardDatentypen_Operationen/3.1_Datentest.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+#include <cstdio>
+
+using namespace std;
+
+int main(void)
+{
+    float r, s, t;
+    r = 50e-3;
+    s = 50e-4;
+    t = s*r;
+    cout << endl << "t = " << t;
+    getchar();
+    return 0;
+}
