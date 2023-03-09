00166b69243ef29276ad4cf39ca7a4ddbc19a958
Simon <simi.wiesinger@gamil.com>
2023-03-09 18:16:40 +0100

    small extension of Example 3.1

--- 01_Cpp_for_Study_and_Work/03_StandardDatentypen_Operationen/3.1_Datentest.cpp
+++ 01_Cpp_for_Study_and_Work/03_StandardDatentypen_Operationen/3.1_Datentest.cpp
@@ -9,6 +9,8 @@ int main(void)
     r = 50e-3;
     s = 50e-4;
     t = s*r;
+    cout << endl << "r = " << r;
+    cout << endl << "s = " << s;
     cout << endl << "t = " << t;
     getchar();
     return 0;
